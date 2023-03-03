57d77aa4f5b848395ec297dcc183833aa97e5a31
deonii <fishingman99@naver.com>
2023-03-03 18:08:28 +0900

    기본 CRUD 기능 및 검색, 페이지 기능 게시판

---
+++ src/main/java/com/study/boardstudy/BoardstudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.boardstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/boardstudy/BoardstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.boardstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
